37d4f7ad0df6d398e17faf790d8fc13593e20238
glop glop <vincent.le.hen@gmail.com>
2022-05-05 22:34:41 +0200

    async movements

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const chevronParent = document.getElementById('container');
 const canvasWidth = 450;
 const canvasHeight = 450;
-const colNumber = 10;
-const rowNumber = 10;
+const colNumber = 40;
+const rowNumber = 20;
 const colWidth = canvasWidth / colNumber;
 const rowHeight = canvasHeight / rowNumber;
 const lineCoordinates = [];
-const xVariatorBase = colWidth / 2.3;
-let liveVariator = 1;
-let directionVariator = 1;
 let variatorA;
 let variatorB;
-let randomVariator;
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
@@ -24,13 +20,23 @@ function setup() {
 
   for (let i = 0; i < colNumber + 1; i++) {
     for (let j = 0; j < rowNumber; j++) {
-      randomVariator = [getRandomArbitrary(-2, -1), getRandomArbitrary(1, 2)];
+      const randomVariator = [getRandomArbitrary(-20, -1), getRandomArbitrary(1, 20)];
       const randomNeedle = Math.floor(Math.random() * randomVariator.length);
+      // line with 2 vectors,
+      // x value of the 2nd vector as a random value beetween a +/- range
+      // variator blindly choose a variator from the previous range
       lineCoordinates.push({
-        vector1: createVector(colWidth * i, rowHeight * j),
-        vector2: createVector(colWidth * i + getRandomArbitrary(xVariatorBase * randomVariator[0], xVariatorBase * randomVariator[1]), rowHeight * (j + 1)),
+        vector1: createVector(
+          colWidth * i, 
+          rowHeight * j
+        ),
+        vector2: createVector(
+          colWidth * i + getRandomArbitrary(randomVariator[0], randomVariator[1]), 
+          rowHeight * (j + 1)
+        ),
         variator: randomVariator[randomNeedle],
-        limitVariator: getRandomArbitrary(.1, .9)
+        variatorRange: [...randomVariator],
+        directionVariator: 1,
       });
     }
   }
@@ -38,18 +44,26 @@ function setup() {
 
 function draw() {
 
-  background('rgba(255,255,255, .05)');
+  background('rgba(27,27,131, .2)');
   
   for (let i = 0; i < lineCoordinates.length; i++) {
 
+    if (lineCoordinates[i].variator >= lineCoordinates[i].variatorRange[1]) {
+      lineCoordinates[i].directionVariator = -1;
+    } else if (lineCoordinates[i].variator <= lineCoordinates[i].variatorRange[0]) {
+      lineCoordinates[i].directionVariator = 1;
+    }
+
+    // only 1 of 2 vector will have a live variation
     if (i % 2 === 0) {
       variatorA = 0;
-      variatorB = lineCoordinates[i + 1].variator * liveVariator;
+      variatorB = lineCoordinates[i + 1].variator + lineCoordinates[i + 1].directionVariator;
     } else {
-      variatorA = lineCoordinates[i].variator * liveVariator;
+      variatorA = lineCoordinates[i].variator + lineCoordinates[i].directionVariator;
       variatorB = 0;
     }
-    
+    // if this is the first vector of the column we give him the first x position
+    // other will recieve the randomized x value from the previous vector
     const firstX = i % colNumber === 0 ? lineCoordinates[i].vector1.x : lineCoordinates[i - 1].vector2.x;
     line(
       firstX + variatorA, 
@@ -59,14 +73,11 @@ function draw() {
     );
     stroke(129,27,201)
 
+    lineCoordinates[i].variator += lineCoordinates[i].directionVariator;
+
   }
+}
 
-  if (liveVariator >= xVariatorBase) {
-    directionVariator = -1;
-  } else if (liveVariator <= xVariatorBase * -1) {
-    directionVariator = 1;
-  }
-
-  liveVariator += directionVariator;
-
+function mousePressed() {
+  noLoop();
 }
